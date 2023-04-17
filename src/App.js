878d8f0993c19db454840034a68020cdc41eb7b5
matteovcc <matteovcc99@gmail.com>
2023-04-17 17:47:25 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import MyFooter from "./components/MyFooter";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/MyNavbar";
 import MainSection from "./components/MainSection";
-// import ProfileSideBar from "./components/ProfileSideBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className="bg-light">
-        <MyNavbar />
-        {/* <ProfileSideBar /> */}
-        <MainSection />
-        <MyFooter />
-      </div>
+      <BrowserRouter>
+        <div className="bg-light">
+          <MyNavbar />
+          <Routes>
+            <Route path="/" element={<MainSection />} />
+            {/* da aggiungere rotta dinamica /profile:id quando faremo homepage (Ricorda params nella fetch)*/}
+          </Routes>
+          <MyFooter />
+        </div>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Col, Container, Form, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Form,
+  Nav,
+  NavDropdown,
+  Navbar,
+} from "react-bootstrap";
 
 import { AiFillHome } from "react-icons/ai";
 import { HiUsers } from "react-icons/hi";
@@ -25,35 +32,67 @@ const MyNavbar = () => {
           <Navbar.Collapse id="navbarScroll">
             <Col xs={2}>
               <Form className="d-flex bg-white ">
-                <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
               </Form>
             </Col>
-            <Nav className="ms-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
-              <Nav.Link href="#home" className="d-flex flex-column align-items-center ">
+            <Nav
+              className="ms-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link
+                href="#home"
+                className="d-flex flex-column align-items-center "
+              >
                 <AiFillHome fontSize={25} />
                 <p style={{ fontSize: 12 }}>Home</p>
               </Nav.Link>
-              <Nav.Link href="#MyNetwork" className="d-flex flex-column align-items-center">
+              <Nav.Link
+                href="#MyNetwork"
+                className="d-flex flex-column align-items-center"
+              >
                 <HiUsers fontSize={25} />
                 <p style={{ fontSize: 12 }}>My Network</p>
               </Nav.Link>
-              <Nav.Link href="#Jobs" className="d-flex flex-column align-items-center">
+              <Nav.Link
+                href="#Jobs"
+                className="d-flex flex-column align-items-center"
+              >
                 <MdWork fontSize={25} />
                 <p style={{ fontSize: 12 }}>Jobs</p>
               </Nav.Link>
-              <Nav.Link href="#Messging" className="d-flex flex-column align-items-center">
+              <Nav.Link
+                href="#Messging"
+                className="d-flex flex-column align-items-center"
+              >
                 <TbMessageCircle fontSize={25} />
                 <p style={{ fontSize: 12 }}>Messaging</p>
               </Nav.Link>
-              <Nav.Link href="#Notifications" className="d-flex flex-column align-items-center">
+              <Nav.Link
+                href="#Notifications"
+                className="d-flex flex-column align-items-center"
+              >
                 <IoMdNotifications fontSize={25} />
                 <p style={{ fontSize: 12 }}>Notifications</p>
               </Nav.Link>
-              <NavDropdown className="border-end" title="Me" id="navbarScrollingDropdown">
+              <NavDropdown
+                className="border-end"
+                title="Me"
+                id="navbarScrollingDropdown"
+              >
                 <NavDropdown.Item href="#action3">Account</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Try premium for free</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">
+                  Try premium for free
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">Settings & Privacy </NavDropdown.Item>
+                <NavDropdown.Item href="#action5">
+                  Settings & Privacy{" "}
+                </NavDropdown.Item>
                 <NavDropdown.Item href="#action5">Help </NavDropdown.Item>
                 <NavDropdown.Item href="#action5">Language</NavDropdown.Item>
               </NavDropdown>
@@ -62,10 +101,14 @@ const MyNavbar = () => {
                 <NavDropdown.Item href="#action3">Jobs</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">Messaging</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">Notifications</NavDropdown.Item>
+                <NavDropdown.Item href="#action5">
+                  Notifications
+                </NavDropdown.Item>
               </NavDropdown>
 
-              <Nav.Link className="text-decoration-underline">Try premium Free</Nav.Link>
+              <Nav.Link className="text-decoration-underline text-warning">
+                Try premium Free
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
