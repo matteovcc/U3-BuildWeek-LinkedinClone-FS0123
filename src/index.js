94a13ed99f7d9a8485f34c30a64d8df663b0bdd6
matteovcc <matteovcc99@gmail.com>
2023-04-17 15:22:36 +0200

    edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "./style/index.css";
 import App from "./App";
+import { store, persistor } from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
